e306164f0b50a3cf27e88290e35d2272fc0f898d
s0mnaths <somnaths027@gmail.com>
2021-04-29 22:31:55 +0530

    Change default value of code field
    Earlier when I had created the models.py , I put the default value of code field as blank, but having it as the function I defined earlier is correct.

--- music_party/api/models.py
+++ music_party/api/models.py
@@ -3,7 +3,7 @@ import string, random
 
 # Create your models here.
 
-def generate_new_code():
+def generate_unique_code():
     length = 6
     while True:
         code = "".join(random.choices(string.ascii_uppercase, k=length))
@@ -12,7 +12,7 @@ def generate_new_code():
     return code
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
