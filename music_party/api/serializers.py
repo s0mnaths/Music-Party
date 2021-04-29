fead6cad1718a5991ae637eac624e94c14c69eae
s0mnaths <somnaths027@gmail.com>
2021-04-29 23:57:52 +0530

    Handle POST requests of CreateRoom

--- music_party/api/serializers.py
+++ music_party/api/serializers.py
@@ -6,3 +6,7 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
         
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
