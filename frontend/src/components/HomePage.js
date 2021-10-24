84a923550b6800909a50733f360c7dbdfe21dc5a
s0mnaths <somnathsharmaji05@gmail.com>
2021-10-24 17:39:40 +0530

    HomePage: Add buttons

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import JoinRoomPage from "./JoinRoomPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,12 +16,34 @@ export default class HomePage extends Component {
     super(props);
   }
 
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            Music Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={ Link }>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={ Link }>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    )
+  }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/">
-            <p>This is the home page</p>
+            { this.renderHomePage() }
           </Route>
           <Route path="/join" component={JoinRoomPage} />
           <Route path="/create" component={CreateRoomPage} />
