935a50cb6299c94655c070850621f58bdfe6945b
s0mnaths <somnathsharmaji05@gmail.com>
2021-10-31 01:33:09 +0530

    Create API endpoint for leaving button

--- api/views.py
+++ api/views.py
@@ -89,3 +89,14 @@ class UserInRoom(APIView):
 
         return JsonResponse(data, status=status.HTTP_200_OK)
         
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host = host_id)
+            if len(room_results) > 0:
+                room = room_results[0]
+                room.delete()
+
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
