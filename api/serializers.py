c54e36a17be6b2de456452151e6e23765315b8a5
s0mnaths <somnathsharmaji05@gmail.com>
2021-11-02 23:07:14 +0530

    API: Update room feature

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import serializers, validators
 from .models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
@@ -10,3 +10,10 @@ class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('guest_can_pause', 'votes_to_skip')
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(validators = [])
+
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
-    path('leave-room', LeaveRoom.as_view())
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view())
 ]
