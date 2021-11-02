db96acd3d171150b3a47e2412dcbb6b988c98433
s0mnaths <somnathsharmaji05@gmail.com>
2021-11-03 00:54:59 +0530

    Settings button: add functionality

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Button, Typography } from '@material-ui/core';
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class Room extends Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class Room extends Component {
     this.getRoomDetails();
     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
     this.updateShowSettings = this.updateShowSettings.bind(this);
+    this.renderSettingsButton = this.renderSettingsButton.bind(this);
+    this.renderSettings = this.renderSettings.bind(this);
   }
 
   getRoomDetails() {
@@ -51,6 +54,26 @@ export default class Room extends Component {
     });
   }
 
+  renderSettings() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <CreateRoomPage update={true} 
+            votesToSkip={this.state.votesToSkip} 
+            guestCanPause={this.state.guestCanPause} 
+            roomCode={this.state.roomCode} 
+            updateCallback={ () => {} }/>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" onClick={ () => this.updateShowSettings(false) }>
+            Close
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+
   renderSettingsButton() {
     return (
       <Grid item xs={12} align="center">
@@ -62,6 +85,9 @@ export default class Room extends Component {
   }
 
   render() {
+    if (this.state.showSettings) {
+      return this.renderSettings();
+    }
     return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
