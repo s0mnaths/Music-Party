d1bfbb5f60b83753fcbb3ca4503bea3c943acdbe
s0mnaths <somnathsharmaji05@gmail.com>
2021-11-11 02:03:44 +0530

    spotify: add callback

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import index
 
+app_name = 'frontend'
+
 urlpatterns = [
-    path('', index),
+    path('', index, name=''),
     path('join', index),
     path('create', index),
     path('room/<str:roomCode>', index)
--- spotify/views.py
+++ spotify/views.py
@@ -1,10 +1,11 @@
 from django.http import response
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .credentials import REDIRECT_URI, CLIENT_ID, CLIENT_SECRET
 from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
+from .util import update_or_create_user_tokens
 
 class AuthURL(APIView):
     def get(self, request, format=None):
@@ -36,3 +37,10 @@ def spotify_callback(request, format=None):
     refresh_token = response.get('refresh_token')
     expires_in = response.get('expires_in')
     error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+    redirect('frontend:')
