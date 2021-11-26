e4ec2f015bede538eee20d5bf19e5450ba99bca2
s0mnaths <somnathsharmaji05@gmail.com>
2021-11-26 18:08:21 +0530

    Create vote model

--- api/models.py
+++ api/models.py
@@ -17,3 +17,4 @@ class Room(models.Model):
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    current_song = models.CharField(max_length=50, null=True)
--- spotify/models.py
+++ spotify/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from api.models import Room
+
 
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50, unique=True)
@@ -7,3 +9,10 @@ class SpotifyToken(models.Model):
     access_token = models.CharField(max_length=150)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
+
+
+class Vote(models.Model):
+    user = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    song_id = models.CharField(max_length=50)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
